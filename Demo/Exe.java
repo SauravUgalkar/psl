0a5033f9ef24d624dfe29fb62f5f107848102f48
saurav <saurav@gmail.com>
2026-03-13 10:31:34 +0000

    Initial commit

---
+++ Demo/Exe.java
@@ -0,0 +1,9 @@
+public class Exe{
+    public static void main(String args[]){
+        Hello obj = new Hello();
+        for(int i=1 ; i<=10;i++){
+            System.out.println(obj.a*i);
+            
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main{
+    public static void main(String ar[]){
+        Sum obj = new Sum();
+        System.out.println(obj.a + "+" + obj.b + "=" + obj.c);
+    }
+}
