ac825da4949936a32a2ac0b92f3c542dd117b5ae
saurav <saurav@gmail.com>
2026-03-13 10:32:36 +0000

    deploy

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
-
-public class Main{
+class Main{
     public static void main(String ar[]){
         Sum obj = new Sum();
-        System.out.println(obj.a + obj.b +"=" + obj.c);
+        System.out.println(obj.a + "+" + obj.b + "=" + obj.c);
     }
 }
-
